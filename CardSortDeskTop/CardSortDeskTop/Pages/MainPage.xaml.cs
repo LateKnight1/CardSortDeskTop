2940e70e0abe1792ad96bc52f30e9add631af3f8
dmcphers <dmcphers@1800contacts.com>
2019-08-09 12:54:53 -0600

    fixed errors

--- CardSortDeskTop/CardSortDeskTop/Pages/MainPage.xaml.cs
+++ CardSortDeskTop/CardSortDeskTop/Pages/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using CardSortDeskTop.Structure;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
