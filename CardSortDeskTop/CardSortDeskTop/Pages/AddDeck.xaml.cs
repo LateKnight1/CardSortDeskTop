e292f1df037070a9fcb8883f8f2abe75245c164f
Duncan <schlussan.duncel@gmail.com>
2019-08-09 12:27:49 -0600

    #1e9x4v[Dev In Progress] Added Add Deck

---
+++ CardSortDeskTop/CardSortDeskTop/Pages/AddDeck.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CardSortDeskTop.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddDeck : Page
+    {
+        public AddDeck()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavView_PaneOpening(Microsoft.UI.Xaml.Controls.NavigationView sender, object args)
+        {
+
+        }
+
+        private void NavView_PaneClosing(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewPaneClosingEventArgs args)
+        {
+
+        }
+
+        private void NavView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+
+        }
+    }
+}
--- CardSortDeskTop/CardSortDeskTop/Pages/MainPage.xaml.cs
+++ CardSortDeskTop/CardSortDeskTop/Pages/MainPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,11 +12,13 @@ namespace CardSortDeskTop
     public sealed partial class MainPage : Page
     {
         public List<string> deckNames { get; private set; }
+        public List<Card> cardsList { get; private set; }
         public MainPage()
         {
             this.InitializeComponent();
             navView.IsPaneOpen = false;
             deckNames = new List<string>();
+            cardsList = new List<Card>();
         }
 
         private void Page_Loading(FrameworkElement sender, object args)
