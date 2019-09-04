02c816af6f812d72c001c8e74230dc7244d04e25
Duncan <schlussan.duncel@gmail.com>
2019-09-04 08:47:23 -0600

    working version

---
+++ CardSortDeskTop/CardSortDeskTop/Structure/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardSortDeskTop.Structure
+{
+    public class Deck
+    {
+        public Deck(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; }
+        public string Type { get; }
+    }
+}
